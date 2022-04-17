db9b33780253e3d2ddfe35eaf1b8cc985e88d835
Stewart Newnham <git@newnham.com>
2022-04-17 09:21:02 +0800

    Added comments tp code

--- Chapter03-LED_Strip/led_strip.py
+++ Chapter03-LED_Strip/led_strip.py
@@ -4,9 +4,9 @@ import time
 ledPins = [11, 12, 13, 15, 16, 18, 22, 3, 5, 24]
 
 def setup():
-    GPIO.setmode(GPIO.BOARD)
-    GPIO.setup(ledPins, GPIO.OUT)
-    GPIO.output(ledPins, GPIP.OUT)
+    GPIO.setmode(GPIO.BOARD) # use Physical GPIO Numbering
+    GPIO.setup(ledPins, GPIO.OUT) # set all ledPins to OUTPUT mode
+    GPIO.output(ledPins, GPIO.HIGH) # All LEDs are initially start with lights on.
 
 def loop():
     while True:
@@ -14,8 +14,18 @@ def loop():
             GPIO.output(pin, GPIO.LOW)
             time.sleep(0.1)
             GPIO.output(pin, GPIO.HIGH)
-        for pin in ledPins[::-1]:
+        # [::-1] => an unspecified start and finish implies implies the whole list. And -1 means to count backwards.
+        for pin in ledPins[::-1]: 
             GPIO.output(pin, GPIO.LOW)
-            time.sleep()
+            time.sleep(0.1)
 
+def destroy():
+    GPIO.cleanup()
     
+if __name__ == '__main__':
+    print('Program is starting....')
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt: 
+        destroy()
